857029b21b02ffaf3768adef91b45525b74b570b
Jikku Jose <jikkujose@gmail.com>
2018-03-19 07:33:00 +0530

    Convert to wide Gifs

--- src/library/PhyGi/index.js
+++ src/library/PhyGi/index.js
@@ -33,9 +33,9 @@ export default class PhyGi extends React.Component {
               >
                 <Gifs>
                   {gifs &&
-                    extract(gifs, "fixed_height_small").map((gif, i) =>
+                    extract(gifs, "fixed_height_small").map((gif, i) => (
                       <Gif url={gif.url} onClick={onSelect(gif.id)} key={i} />
-                    )}
+                    ))}
                 </Gifs>
               </ScrollArea>
             </div>
@@ -57,11 +57,11 @@ PhyGi.propTypes = {
 const gutter = 0.232
 
 const Gif = styled.div.attrs({
-  className: "pointer grow",
+  className: "pointer",
 })`
   margin-right: ${gutter}rem;
   margin-bottom: ${gutter}rem;
-  width: 4.8rem;
+  width: 100%;
   height: 4.8rem;
   background: url(${p => p.url});
   background-size: cover;
@@ -70,5 +70,4 @@ const Gif = styled.div.attrs({
 
 const Gifs = styled.div.attrs({
   className: "flex flex-wrap",
-})`
-`
+})``
